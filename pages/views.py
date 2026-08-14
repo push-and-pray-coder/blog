6b57bb6d9f5998b191e806d7516c496733f09005
push-and-pray-coder <Titan.Chen@sdgku.edu>
2026-08-13 20:12:59 -0700

    Class 2 Examples

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.shortcuts import HttpResponse, render
 
 # Create your views here.
 # Function Based Views vs. Class Based Views
@@ -7,3 +8,7 @@ class HomePageView(TemplateView):
     template_name = "pages/home.html"
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
+# Function Based Views
+def contact_page(request):
+    # return HttpResponse("Hellow World from a FBV")
+    return render(request, "pages/contact.html")
